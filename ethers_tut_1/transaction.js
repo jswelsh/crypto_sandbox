547d3300b875aa3855484245f61bc7f1bc2d5c2f
jswelsh <james.sinclair.welsh@gmail.com>
2021-03-19 19:12:12 -0700

    ignore

--- ethers_tut_1/transaction.js
+++ ethers_tut_1/transaction.js
@@ -26,9 +26,9 @@ const provider = new ethers.providers.StaticJsonRpcProvider(
 
 // Variables
 const account_from = {
-    privateKey: 'YOUR-PRIVATE-KEY-HERE',
+    privateKey: '99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342'/* 'YOUR-PRIVATE-KEY-HERE', */
 };
-const addressTo = 'ADDRESS-TO-HERE';
+const addressTo = '0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b'/* 'ADDRESS-TO-HERE'; */
 
 // Create Wallet
 let wallet = new ethers.Wallet(account_from.privateKey, provider);
